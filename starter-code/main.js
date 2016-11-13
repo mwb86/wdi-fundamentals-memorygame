37c9ff9c9412cd671f83cfd899505f8dab1a3f36
Mason Brown <masonwbrown@gmail.com>
2016-11-13 18:21:48 -0500

    javascript creates cards now

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,14 @@
-console.log("JS file is connected to HTML! Woo!");
-var cardOne= "queen";
-var cardTwo="queen";
-var cardThree = "king";
-var cardFour = "king";
-if(cardTwo === cardFour){
-  alert( "Try again!");
-}
-else if(cardTwo === cardThree){
-  alert("Try again");
-}
-else if(cardOne === cardTwo){
-  alert ("You've found a match!");
-}
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+
+  for (var i = 0; i < 4; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = 'card';
+    gameBoard.appendChild(newCard);
+  }
+};
+
+createCards();
